a26e6a780097a3152dd4b612484cdbc9d74b6b49
BarryS <barry.sunderland@outlook.com>
2020-05-14 20:31:39 +0200

    first

---
+++ train_model.py
@@ -0,0 +1,20 @@
+#%%
+! pip install matplotlib
+! pip install pandas
+! pip numpy
+
+
+
+#%%
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+x = np.arange(0,100)
+y = x**2
+
+plt.scatter(x,y)
+plt.show()
+
+
+# %%
